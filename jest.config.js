a663243e4f6bac75ba4fe99e82f38806e517238d
Carlos Raez <carlosraez@gmail.com>
2022-08-16 09:13:38 +0200

    Revert "adding jest to proyect"
    This reverts commit f00a72beba77acddefd3491339566a421383e175.

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    testEnvironment: 'jest-environment-jsdom',
-    setupFiles: ['./jest.setup.js'],
-    transformIgnorePatterns: [],
-    
-    moduleNameMapper: {
-        '\\.(css|sass)$': 'sass/tests/mocks/styleMock.js',
-    },
-}

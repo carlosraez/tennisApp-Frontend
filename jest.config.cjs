b788f1c4757730566900ed54bdf1bad6fddfa48b
Carlos Raez <carlosraez@gmail.com>
2022-09-01 16:09:26 +0200

    resolve commonjs

--- jest.config.cjs
+++ jest.config.cjs
@@ -10,7 +10,7 @@ module.exports = {
     
     // ModuleNameMapper sólo si ocupamos importar CSS en nuestros componentes para el testing
     moduleNameMapper: {
-        '\\.(css|sass)$': "<rootDir>/test/mocks/styleMock.js",
-        "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/mocks/fileMock.js",
+      "\\.(css|less|scss)$": "identity-obj-proxy",
+      "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/mocks/fileMock.js",
       }
 }
